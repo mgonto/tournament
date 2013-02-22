f6a89f310de5b319e5032ddc27957550536a9bff
Martin Gontovnikas <martin@gonto.com.ar>
2013-02-22 16:33:21 -0300

    Removed tests and active_record completely

--- ruby/app/controllers/v1/user_controller.rb
+++ ruby/app/controllers/v1/user_controller.rb
@@ -3,7 +3,13 @@ class V1::UserController < ApplicationController
 
   before_filter :authenticate_user!, :only => :destroy
   def create
-    if params[:email]
+    if params[:user][:email]
+      @user = User.new(params[:user])
+      if @user.save
+        render json: @user.as_json(except: [:password, :facebook_id, :facebook_token]), status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     else params[:facebook_token]
     else
        raise ArgumentError.new("Email or Facebook token are required")
