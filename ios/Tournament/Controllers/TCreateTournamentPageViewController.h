92150919bca8d6d05010d74308d98a148021213d
Nicolas Purita <nicospuri@gmail.com>
2013-02-24 22:17:52 -0300

    Set Create Tournament Form as Page View Controller

---
+++ ios/Tournament/Controllers/TCreateTournamentPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCreateTournamentPageViewController.h
+//  Tournament
+//
+//  Created by Nicolas Purita on 2/24/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCreateTournamentPageViewController : UIViewController
+
+@end
