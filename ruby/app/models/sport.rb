59ece7a601426d90b156ed93d98104574c58a3c9
Martin Silva <martinsilva@zauberlabs.com>
2013-03-16 15:56:30 -0300

    add model validations for sport

--- ruby/app/models/sport.rb
+++ ruby/app/models/sport.rb
@@ -4,4 +4,9 @@ class Sport
   field :draw_points, type: Integer
   field :losing_points, type: Integer
   field :name, type: String
+
+  validates :winning_points, :presence => true, :numericality => { :only_integer => true }
+  validates :losing_points, :presence => true, :numericality => { :only_integer => true }
+  validates :draw_points, :allow_nil => true, :numericality => { :only_integer => true }
+  
 end
--- ruby/spec/models/sport_spec.rb
+++ ruby/spec/models/sport_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Sport do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_numericality_of(:winning_points) }
+  it { should validate_numericality_of(:draw_points) }
+  it { should validate_numericality_of(:losing_points) }
+
+  it { should allow_value(nil).for(:draw_points) }
+  it { should_not allow_value(nil).for(:winning_points) }
+  it { should_not allow_value(nil).for(:losing_points) }
 end
