1b855088e2be699c6cc7f280e10cd9b0b6250ea6
Martin Silva <martinsilva@zauberlabs.com>
2013-03-16 15:45:50 -0300

    add sport_template validations

--- ruby/app/models/sport_template.rb
+++ ruby/app/models/sport_template.rb
@@ -1,3 +1,8 @@
 class SportTemplate < Sport
   include Mongoid::Document
+
+  validates :winning_points, :presence => true, :numericality => { :only_integer => true }
+  validates :losing_points, :presence => true, :numericality => { :only_integer => true }
+  validates :draw_points, :allow_nil => true, :numericality => { :only_integer => true }
+
 end
