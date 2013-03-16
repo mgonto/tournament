4f6a5f441cedd425643c2b76d257c7e7140ac96f
Martin Silva <martinsilva@zauberlabs.com>
2013-03-16 15:46:26 -0300

    add shoulda matchers

--- ruby/spec/models/sport_template_spec.rb
+++ ruby/spec/models/sport_template_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe SportTemplate do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+   it { should validate_numericality_of(:winning_points) }
+   it { should validate_numericality_of(:draw_points) }
+   it { should validate_numericality_of(:losing_points) }
+
+   it { should allow_value(nil).for(:draw_points) }
+   it { should_not allow_value(nil).for(:winning_points) }
+   it { should_not allow_value(nil).for(:losing_points) }
+
 end
