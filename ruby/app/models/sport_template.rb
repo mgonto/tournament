a65dcf661d3660b425f7ca72702da5a6676ecd28
Martin Gontovnikas <martin@gonto.com.ar>
2013-03-27 20:12:01 -0300

    Removed validations al pedo

--- ruby/app/models/sport_template.rb
+++ ruby/app/models/sport_template.rb
@@ -1,8 +1,4 @@
 class SportTemplate < Sport
   include Mongoid::Document
 
-  validates :winning_points, :presence => true, :numericality => { :only_integer => true }
-  validates :losing_points, :presence => true, :numericality => { :only_integer => true }
-  validates :draw_points, :allow_nil => true, :numericality => { :only_integer => true }
-
 end
